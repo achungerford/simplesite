c4ca016df523273d0f6a3f0f37c2d3e52c14965f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-19 14:41:56 -0700

    ch2 setup and first commit

---
+++ ch2/script.js
@@ -0,0 +1,11 @@
+/****************************************
+ * Comments in js look like C comments
+ ***************************************/
+
+// Single line comments in js also look like C comments
+
+ /*
+  Varialbes & Data Types
+ */
+
+ 
