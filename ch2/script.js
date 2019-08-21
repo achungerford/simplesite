628c0135fc401fc164930d0d792475dcd5574c26
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-21 01:31:41 -0700

    gitignore

--- ch2/script.js
+++ ch2/script.js
@@ -12,5 +12,18 @@ var firstName = 'Michael';
 console.log(firstName);
 
 var lastName = 'Jordan';
+var age = 56;
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'CEO';
+console.log(job);
+
+
+
 console.log(lastName);
  
