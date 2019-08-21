47d853c506408ca6b5463e83e61b96839174b9a4
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-21 03:00:49 -0700

    push from master

---
+++ ch3/script.js
@@ -0,0 +1,8 @@
+/****************************************
+ * Comments in js look like C comments
+ ***************************************/
+
+// Single line comments in js also look like C comments
+
+
+
