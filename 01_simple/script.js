a56663af9c19eb051e791305e47408ebd46e7991
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-21 21:47:20 -0700

    combined 01

--- 01_simple/script.js
+++ 01_simple/script.js
@@ -11,3 +11,6 @@ window.alert("Here is my message.");
 
 // since the window object is atop the DOM heirarchy you can leave off window
 alert("My second message with more words to make it obvious they're different.");
+
+// the write method: writes directly into the DOM
+document.write("Here is another message.");
