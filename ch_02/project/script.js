dfba0204d6f91ad6e4c792a825cb5374a07e7ee0
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-22 13:15:41 -0700

    temperature demo

---
+++ ch_02/project/script.js
@@ -0,0 +1,9 @@
+// temperature in Celsius
+var cTemp = 100;
+
+// conversion to Fahrenheit
+var hTemp = ((cTemp * 9) / 5) + 32;
+
+// display results
+document.write("Temperature in Celsius: " + cTemp + " degrees<br/>");
+document.write("Temperature in Fahrenheit: " + hTemp + " degrees.");
