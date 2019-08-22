260ac3810e92b16d05ca1a6ff64199be32738963
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-21 21:13:12 -0700

    two alerts

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /* comments look */
 // like C comments
 
+
+/*
+Methods associated with the the window & document objects of the DOM
+==================================================================== */
+
+// Talking to the user: alert
+window.alert("Here is my message.");
+
+// since the window object is atop the DOM heirarchy you can leave off window
+alert("My second message with more words to make it obvious they're different.");
