540c0a5097da68d1649b35ae78259b2b6f200b4c
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-08-22 12:26:25 -0700

    declare types in variables

---
+++ ch_02/script.js
@@ -0,0 +1,13 @@
+// Reading a property of the "document" object.
+// Read <title> and use it an alert.
+alert(document.title);
+
+// Using the "write" method to add a sentence to the "document" object.
+document.write("The quick brown fox jumps over the lazy dog.");
+
+
+/* Variables in JS
+====================================*/
+var productName = "Leather wallet";
+
+alert(productName);
